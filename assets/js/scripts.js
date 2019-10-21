d6e6d0eb39bf037c550e8b531fda2814b1fd4f16
Ubuntu <ubuntu@ip-172-31-93-234.ec2.internal>
2019-10-21 07:48:35 +0000

    edit colors and add in button functions

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,6 +1,6 @@
-let tiles = Array.from(document.querySelectorAll('.tiles'));
+let blocks = Array.from(document.querySelectorAll('.block'));
 let buttonDiv = document.getElementById('btn');
-let tilesLength = tiles.length;
+let blocksLength = blocks.length;
 let score = 0;
 
 // First function of the play button
@@ -29,83 +29,99 @@ function onClick() {
         }
     }, 1000);
 
-    // Shuffles the tiles array into a random order
-    let i = tilesLength,
+    // Shuffles the blocks array into a random order
+    let i = blocksLength,
         rand, temp;
     while (--i > 0) {
         rand = Math.floor(Math.random() * (i + 1));
-        temp = tiles[rand];
-        tiles[rand] = tiles[i];
-        tiles[i] = temp;
+        temp = blocks[rand];
+        blocks[rand] = blocks[i];
+        blocks[i] = temp;
     }
 
     // Selects half of the shuffled array and adds the color class
-    let bottomHalfTiles = tiles.slice(tilesLength / 2, tilesLength);
-    bottomHalfTiles.forEach(function(i) {
-        i.classList += (' green');
+    let bottomHalfblocks = blocks.slice(blocksLength / 2, blocksLength);
+    bottomHalfblocks.forEach(function(i) {
+        i.classList.replace('blue', 'green');
     });
 
     // Replaces the color class with an unused class after 6 seconds
     function clearBoard() {
-        bottomHalfTiles.map(function(i) {
-            i.classList.replace('green', 'in-play');
+        bottomHalfblocks.map(function(i) {
+            i.classList.replace('green', 'blue');
+            i.classList.add('marker')
         });
     }
     setTimeout(clearBoard, 6000);
-    console.log(tiles);
+    console.log(blocks);
 
     // Activates the users ability to click and change the block color 6 seconds after the start button is pressed
     function allowUserClick() {
-        tiles.forEach(function(i) {
+        blocks.forEach(function(i) {
             i.addEventListener('click', tileClicked);
         });
 
         function tileClicked() {
             this.classList.toggle('green');
+            this.classList.toggle('blue');
         }
     }
     setTimeout(allowUserClick, 6000);
 }
 
-
+let modalBtn1 = document.getElementById('modal-btn1');
+let modalBtn2 = document.getElementById('modal-btn2');
 // Function checks the users answers on clicking the check button and displays a modal
 function checkAttempt() {
-    let modalBtn = document.getElementById('modal-btn');
-    
-    // Loops through the tiles array and increments 1 to the score if a condition is true
+
+    // Loops through the blocks array and increments 1 to the score if a condition is true
     function checkClasses() {
-        tiles.forEach(function(it) {
-            if ((it.classList.contains('in-play')) && (it.classList.contains('green'))) {
+        blocks.forEach(function(it) {
+            if ((it.classList.contains('marker')) && (it.classList.contains('green'))) {
                 score++;
             }
-            else if ((it.classList.contains('tiles')) && (it.classList.contains('green')) && !(it.classList.contains('in-play'))) {
-                score = tilesLength;
+            else if ((it.classList.contains('green')) && !(it.classList.contains('marker'))) {
+                score = blocksLength;
             }
         });
     }
     checkClasses();
-    
+
     // Modal either gives a yes or no answer as to whether the user has succeeded
-    if (score == tilesLength / 2) {
+    if (score == blocksLength / 2) {
         document.getElementById('modal-head').textContent = ('CONGRATULATIONS!');
-        document.getElementById('modal-text').textContent = ('You did it! why not do it again');
-        modalBtn.innerHTML = '<button type="button" class="btn btn-primary" onclick="location.reload();">Play Again</button>';
-    }else {
+        modalBtn1.innerHTML = '<button type="button" class="btn btn-primary" onclick="location.reload();">New Game</button>';
+        modalBtn2.innerHTML = '';
+    }
+    else {
         document.getElementById('modal-head').textContent = ('SORRY NOT THIS TIME!');
-        document.getElementById('modal-text').textContent = ('See why and try again');
-        modalBtn.innerHTML = '<button type="button" class="btn btn-primary" data-dismiss="modal" onclick="reveal();">Reveal</button>';
+        modalBtn1.innerHTML = '<button type="button" class="btn btn-primary" data-dismiss="modal" onclick="revealAttempt();">Reveal</button>';
+        modalBtn2.innerHTML = '<button type="button" class="btn btn-primary" data-dismiss="modal" onclick="tryAgain();">Try Again</button>';
     }
     // resets the score to 0 to keep the users attempt accurate
     score = 0;
 }
 
-function reveal(){
-    buttonDiv.innerHTML = '<button type="button" class="btn btn-primary btn-lg" id="play-btn" onclick="location.reload();">Play Again</button>';
-    tiles.forEach(function(it){
-        if( (it.classList.contains('green'))&& !(it.classList.contains('in-play')) ){
-            it.classList.remove('green');
-        }else if ( !(it.classList.contains('green'))&& (it.classList.contains('in-play')) ){
-            it.classList.add('green');
+function tryAgain() {
+    blocks.forEach(function(it) {
+        if (it.classList.contains('green')) {
+            it.classList.replace('green', 'blue');
         }
     });
+}
+
+// Replaces green class with blue class
+function revealAttempt() {
+    setInterval(revealCorrect, 2000);
+    buttonDiv.innerHTML = '<button type="button" class="btn btn-primary btn-lg" id="play-btn" onclick="location.reload();">New Game</button>';
+    function revealCorrect(){
+    blocks.forEach(function(it) {
+        if ((it.classList.contains('green')) && !(it.classList.contains('marker'))) {
+            it.classList.replace('green', 'blue');
+        }
+        else if (!(it.classList.contains('green')) && (it.classList.contains('marker'))) {
+            it.classList.replace('blue', 'green');
+        }
+    });
+}
 }
